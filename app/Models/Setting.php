6cde73963f4e583544ea84b40d22c6e2cf747fd2
Jonathan Baiak <jonathanbaiak@gmail.com>
2025-12-23 17:03:50 -0300

    Seeder para popular o settings com os dados da radio

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+  use HasFactory;   //
+  protected $fillable = [
+        'radio_name',
+        'slogan',
+        'frequency',
+        'streaming_url',
+        'contact_email',
+        'whatsapp_number',
+    ];
 }
--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -15,6 +15,10 @@ public function run(): void
     \App\Models\Setting::updateOrCreate(['id' => 1], [
         'radio_name' => 'Porto União FM',
         'slogan' => 'A rádio da nossa gente',
+        'frequency' => '87.9 FM',
+        'streaming_url' => 'https://stm4.xcast.com.br:12006/stream',
+        'contact_email' => 'direcao879@hotmail.com',
+        'whatsapp_number' => '(42)99123-3360',
     ]);
 }
 }
