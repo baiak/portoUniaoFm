c565f4a0cad81c7ed8645d4efabd7ecbafb22dae
Jonathan Baiak <jonathanbaiak@gmail.com>
2025-12-23 17:16:32 -0300

    Adicionado logo_path na tabela setting

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\FileUpload;
 
 class Setting extends Model
 {
   use HasFactory;   //
   protected $fillable = [
+        'logo_path',
         'radio_name',
         'slogan',
         'frequency',
