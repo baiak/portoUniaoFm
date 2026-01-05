452e4efca9bed9f8f999e48bb061303eaf7ef357
Jonathan Baiak <jonathanbaiak@gmail.com>
2026-01-05 11:38:19 -0300

    Sortable e toggle para ativar e desativar banner configurados

--- app/Filament/Resources/Banners/Tables/BannersTable.php
+++ app/Filament/Resources/Banners/Tables/BannersTable.php
@@ -8,6 +8,8 @@ use Filament\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\ToggleColumn;
 
 class BannersTable
 {
@@ -15,14 +17,21 @@ class BannersTable
     {
         return $table
             ->columns([
+                ImageColumn::make('imagem_path')
+                     ->label('Banner')
+                     ->square()
+                     ->width(100)
+                     ->disk('public'),
                 TextColumn::make('titulo')
-                    ->searchable(),
-                TextColumn::make('imagem_path')
                     ->searchable(),
                 TextColumn::make('link_url')
                     ->searchable(),
-                IconColumn::make('esta_ativo')
-                    ->boolean(),
+                ToggleColumn::make('ativo')
+                    ->label('Status')
+                    ->onIcon('heroicon-m-check')
+                    ->offIcon('heroicon-m-x-mark')
+                    ->onColor('success')
+                    ->offColor('danger'),
                 TextColumn::make('ordem')
                     ->numeric()
                     ->sortable(),
@@ -35,6 +44,8 @@ class BannersTable
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->reorderable('ordem')
+            ->defaultSort('ordem', 'asc')
             ->filters([
                 //
             ])
--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -3,14 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class Banner extends Model
+
+class Banner extends Model implements Sortable
 {
+    use SortableTrait;
+
     protected $fillable = [
         'titulo',
         'imagem_path',
         'link_url',
         'esta_ativo',
         'ordem',
+    ];
+
+    public $sortable = [
+        'order_column_name' => 'ordem',
+        'sort_when_creating' => true,
     ];
 }
