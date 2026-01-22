d9a1ce9b8a2b8c5cc6bd418eb921655a5a5087fb
Jonathan Baiak <jonathanbaiak@gmail.com>
2026-01-22 09:34:46 -0300

    revertendo mudanca no upload de imagem do banner, tirando optimize

--- app/Filament/Resources/Banners/Schemas/BannerForm.php
+++ app/Filament/Resources/Banners/Schemas/BannerForm.php
@@ -22,7 +22,7 @@ class BannerForm
                         ->directory('banners')
                         ->imageEditor()
                         ->disk('public')
-                        ->optimize('webp')
+                        
                         ->visibility('public')
                         ->required(),
 
