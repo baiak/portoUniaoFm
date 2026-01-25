35ce0fd74a15cfd3e3c7bdfb9ddc11cfc30a3c61
VPS Deploy <jonathanbaiak@gmail.com>
2026-01-25 00:09:27 -0300

    Arrumando a casa para o google adsense

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create('https://clube87.com')->writeToFile(public_path('sitemap.xml'));
+        $this->info('Sitemap gerado com sucesso!');
+    }
+}
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-adsense-account" content="ca-pub-6803402036116581">
     <title>{{ $title ?? 'Porto União FM' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     @livewireStyles
     <style>[x-cloak] { display: none !important; }</style>
     @php
